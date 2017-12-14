19afbaae61d222708790e32e8c5279b3629e0582
zhaowei <verglchiu@foxmail.com>
2017-12-14 22:48:28 +0800

    mysql jdbc

--- src/main/scala/spark/HelloSpark.scala
+++ src/main/scala/spark/HelloSpark.scala
@@ -1,11 +1,13 @@
 package spark
 
+import org.apache.spark.deploy.yarn.YarnSparkHadoopUtil
 import org.apache.spark.{SparkConf, SparkContext}
 
 object HelloSpark {
   def main(args: Array[String]): Unit = {
     //初始化sparkconf,设置spark appname，以及master，这两个是必须的
     val conf=new SparkConf().setAppName("HelloSpark").setMaster("local[4]");
+    conf.set("spark.hadoop.yarn.resourcemanager.hostname","ht05")
     //由conf初始化sparkcontext
     val sc=new SparkContext(conf);
     //从hdfs地址读取一个文件
