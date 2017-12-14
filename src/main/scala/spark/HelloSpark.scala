36c14a73c88a17eba245e24633ec03a6529569d3
zhaowei <verglchiu@foxmail.com>
2017-12-14 17:04:44 +0800

    init

---
+++ src/main/scala/spark/HelloSpark.scala
@@ -0,0 +1,25 @@
+package spark
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+object HelloSpark {
+  def main(args: Array[String]): Unit = {
+    //初始化sparkconf,设置spark appname，以及master，这两个是必须的
+    val conf=new SparkConf().setAppName("HelloSpark").setMaster("local[4]");
+    //由conf初始化sparkcontext
+    val sc=new SparkContext(conf);
+    //从hdfs地址读取一个文件
+    val textFile=sc.textFile("hdfs://192.168.2.5:9000/zhaowei/pass.txt")
+    //1.count行数
+    val count=textFile.count()
+    println("count:"+count)
+    //2.计算单词数
+    val flatMapRdd=textFile.flatMap(line=>line.split(" "))
+    println("words count:"+flatMapRdd.count())
+    //3.
+    val flatMapRdd1=textFile.flatMap(line=>line.toCharArray)
+    println("char count:"+flatMapRdd1.count())
+    val char=flatMapRdd1.take(100)
+    println(char)
+  }
+}
