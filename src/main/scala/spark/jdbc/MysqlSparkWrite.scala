74a08eb40a9b3faa72aa765ba81a93cb77c4b21d
vergilchiu <vergilchiu@foxmail.com>
2017-12-15 01:34:59 +0800

    read mysql and write mysql

---
+++ src/main/scala/spark/jdbc/MysqlSparkWrite.scala
@@ -0,0 +1,16 @@
+package spark.jdbc
+
+import org.apache.spark.sql.{DataFrameWriter, SparkSession}
+
+object MysqlSparkWrite {
+  def main(args: Array[String]): Unit = {
+    val sparkSession = SparkSession.builder().appName("MysqlSpark").master("local[2]").getOrCreate()
+    //下面一行的read等于new DataFrameReader(sc).read
+    val data = sparkSession.read.format("jdbc").options(Map("url" -> "jdbc:mysql://localhost:3306/report",
+      "driver" -> "com.mysql.jdbc.Driver",
+      "dbtable" -> "(select * from device_stat order by createtime limit 100) as aaa",
+      "user" -> "root", "password" -> "Zw2051300663211138.")).load()
+    data
+    new DataFrameWriter[DataFrame]()
+  }
+}
